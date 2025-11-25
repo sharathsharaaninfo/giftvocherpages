8943ea9e98d47fca15beb4e22ac3a2fab5a0ff1f
Sharath <sharath@AD.SHARAANINFO.COM>
2025-11-25 16:45:54 +0530

    pages

--- src/App.js
+++ src/App.js
@@ -46,12 +46,14 @@ function App() {
         <Route exact path="/Akshayatritiya" element={<Akshayatritiya/>}/>
         <Route exact path="/Bhimagala" element={<GiftPage path='/Bhimagalaterms.html' bannerImg={BHIMAGALA_IMG.banner} mbBannerImg={BHIMAGALA_IMG.mbBanner}  campaignKey='bhimagala' />} />
         <Route exact path="/Mysuru" element={<GiftPage path='/MysoreTerms.html' bannerImg={MYSURU_IMG.banner} mbBannerImg={MYSURU_IMG.mbBanner} campaignKey='mysuru' />} />
-        <Route exact path="/Madurai" element={<GiftPage path='/MadhuraiTerms.html' bannerImg={MADURAI_IMG.banner} mbBannerImg={MADURAI_IMG.mbBanner} campaignKey='madurai' />} />
+        <Route exact path="/Madhurai" element={<GiftPage path='/MadhuraiTerms.html' bannerImg={MADURAI_IMG.banner} mbBannerImg={MADURAI_IMG.mbBanner} campaignKey='madurai' />} />
         <Route exact path="/Hoodi" element={<GiftPage path='/HoodiTerms.html' bannerImg={HOODI_IMG.banner} mbBannerImg={HOODI_IMG.mbBanner} campaignKey='hoodi' />} />
         <Route exact path="/Hydrabad" element={<GiftPage path='/HyderabadTerms.html' bannerImg={HYD_IMG.banner} mbBannerImg={HYD_IMG.mbBanner} campaignKey='hydrabad' />} />
         <Route exact path="/Generic" element={<GiftPage path='/GenericTerms.html' bannerImg={GENERIC_IMG.banner} mbBannerImg={GENERIC_IMG.mbBanner} campaignKey='generic' />} />
         <Route exact path="/Udupi" element={<GiftPage path='/conditions.html' bannerImg={UDUPI_IMG.banner} mbBannerImg={UDUPI_IMG.mbBanner} campaignKey='udupi' />} />
         <Route exact path="/Hassan" element={<GiftPage path='/HassanTerms.html' bannerImg={HASSAN_IMG.banner} mbBannerImg={HASSAN_IMG.mbBanner} campaignKey='hassan' />} />
+        <Route exact path="/Tvm" element={<GiftPage path='/conditions.html' bannerImg={UDUPI_IMG.banner} mbBannerImg={UDUPI_IMG.mbBanner} campaignKey='udupi' />} />
+        <Route exact path="/Cochin" element={<GiftPage path='/conditions.html' bannerImg={HASSAN_IMG.banner} mbBannerImg={HASSAN_IMG.mbBanner} campaignKey='hassan' />} />
         <Route path="/thank-you" element={<ThankYouPage />} />
         <Route exact path="/" element={
           <div className="content">
